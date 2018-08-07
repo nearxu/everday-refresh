1190f3047843af9f3b280c8aeb63111c71a2067e
nearxu <2448895924@qq.com>
2018-08-07 15:10:39 +0800

    ds

---
+++ August/vscode.js
@@ -0,0 +1,20 @@
+{
+    "workbench.iconTheme": "vscode-icons",
+        "window.zoomLevel": 2,
+            // "files.autoSave": "onFocusChange",
+            "workbench.colorTheme": "Monokai",
+                "guides.enabled": false,
+                    "explorer.confirmDelete": false,
+                        "explorer.confirmDragAndDrop": false,
+                            "path-intellisense.mappings": {
+        "~": "${workspaceRoot}/lib"
+    },
+    "javascript.implicitProjectConfig.experimentalDecorators": true,
+        "javascript.validate.enable": false,
+            "editor.formatOnSave": true,
+                "prettier.eslintIntegration": true,
+                    "git.enableSmartCommit": true,
+                        "vsicons.dontShowNewVersionMessage": true,
+                            "vsicons.projectDetection.disableDetect": true,
+                                "vetur.validation.template": false
+}
